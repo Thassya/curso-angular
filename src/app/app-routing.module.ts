b0b1f83f75ef6406770b2f25e1b3e77c31d2425b
Thassya <thayowisky@gmail.com>
2020-03-18 22:44:51 +0100

    CanLoad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,13 +12,15 @@ const routes: Routes = [
     path: 'cursos',
     loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),  //lazyloading
     canActivate: [AuthGuard],
-    canActivateChild: [CursosGuard]
+    canActivateChild: [CursosGuard],
+    canLoad: [AuthGuard]
   },
   {
     path: 'alunos',
     loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
-    canActivate: [AuthGuard]
-    // ,canActivateChild: [AlunosGuard]
+    canActivate: [AuthGuard],
+    // canActivateChild: [AlunosGuard]
+    canLoad: [AuthGuard]
   },
   { path: 'login', component: LoginComponent },
   {
--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router, CanLoad, Route } from '@angular/router';
 import { Observable } from 'rxjs';
 
 import { AuthService } from './../login/auth.service';
@@ -8,23 +8,38 @@ import { AuthService } from './../login/auth.service';
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | boolean {
 
+    return this.verificarAcesso();
+
+  }
+
+  private verificarAcesso() {
     if (this.authService.usuarioEstaAutenticado())
       return true;
 
     this.router.navigate(['/login']);
     return false;
+  }
+
+  canLoad(
+    route: Route,
+    segments: import("@angular/router").UrlSegment[]
+  ): boolean | Observable<boolean> | Promise<boolean> {
+    console.log('canLoad: verificando se o usuário pode carregar o cod do módulo');
+
+    return this.verificarAcesso();
 
   }
 }
