7dbeac2e2779f1a0431bcecafaf6625296af8f12
Thassya <thayowisky@gmail.com>
2020-03-22 19:52:26 +0100

    pequisa reativa

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,10 +1,9 @@
+import { switchMap } from 'rxjs/operators';
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { map } from 'rxjs/operators';
-
+import { tap, filter, map, distinctUntilChanged, debounceTime } from 'rxjs/operators';
 @Component({
   selector: 'app-lib-search',
   templateUrl: './lib-search.component.html',
@@ -14,28 +13,42 @@ export class LibSearchComponent implements OnInit {
 
   queryFiled = new FormControl();
   readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
-  //'y?fields=name,filename,version';
+  readonly FIELDS = '?fields=name,filename,version'; 
   results$: Observable<any>;
   total: number;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+   this.results$ =  this.queryFiled.valueChanges.pipe(
+     map(value => value.trim()),
+     filter(value => value.length > 3),
+     debounceTime(200),
+     distinctUntilChanged(),     
+     switchMap(value => this.http.get(this.SEARCH_URL,{
+       params: {
+         search: value,
+         fields: this.FIELDS
+       }
+     })),
+     tap((res:any) => this.total = res.total),
+     map((res:any)=> res.results)
+   );
   }
 
   onSerach() {
     let value = this.queryFiled.value;
     if (value && (value = value.trim()) !== '') {
 
-      const fields = '?fields=name,filename,version'; 
+     
       const params_ = {
         search: value,
-        fields: fields
+        fields: this.FIELDS
       }
       //para dinamico
       let params = new HttpParams(); //parecido com URLSearchParams
       params = params.set('search', value);
-      params = params.set('fileds', fields);
+      params = params.set('fileds', this.FIELDS);
 
       this.results$ = this.http.get(this.SEARCH_URL, { params })
         .pipe(
