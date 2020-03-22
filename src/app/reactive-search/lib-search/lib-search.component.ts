83df4e4bc8ed23ff2f519e86501ff85490734631
Thassya <thayowisky@gmail.com>
2020-03-22 19:42:02 +0100

    busca nao reativa funcionando

--- src/app/reactive-search/lib-search/lib-search.component.ts
+++ src/app/reactive-search/lib-search/lib-search.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-lib-search',
@@ -9,13 +13,35 @@ import { FormControl } from '@angular/forms';
 export class LibSearchComponent implements OnInit {
 
   queryFiled = new FormControl();
+  readonly SEARCH_URL = 'https://api.cdnjs.com/libraries';
+  //'y?fields=name,filename,version';
+  results$: Observable<any>;
+  total: number;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
-  onSerach(){
-    
+  onSerach() {
+    let value = this.queryFiled.value;
+    if (value && (value = value.trim()) !== '') {
+
+      const fields = '?fields=name,filename,version'; 
+      const params_ = {
+        search: value,
+        fields: fields
+      }
+      //para dinamico
+      let params = new HttpParams(); //parecido com URLSearchParams
+      params = params.set('search', value);
+      params = params.set('fileds', fields);
+
+      this.results$ = this.http.get(this.SEARCH_URL, { params })
+        .pipe(
+          tap((res: any) => this.total = res.total),
+          map((res: any) => res.results)
+        );
+    }
   }
 }
